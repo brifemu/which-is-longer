71d38440a809762ac13d41ede00cda57efb87484
Brian Fernández Muñiz <brifemu@gmail.com>
2022-05-29 15:04:03 +0200

    Install HLTB dependence

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { HowLongToBeatService } from 'howlongtobeat';
+
+let hltbService = new HowLongToBeatService();
+
 function App() {
+
+  const searchByID = (id) => {
+    let response;
+    hltbService.detail(id).then(result => result = response).catch(e => console.error(e));
+    return response;
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {console.log(searchByID(36936))}
       </header>
     </div>
   );
